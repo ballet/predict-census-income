2e7279c2c1ff31315657a55002bbf03be8196d55
Micah Smith <micahs@mit.edu>
2020-07-22 13:28:02 -0400

    Implement target encoder

--- src/predict_census_income/features/encoder.py
+++ src/predict_census_income/features/encoder.py
@@ -1,10 +1,19 @@
 from ballet.eng.misc import IdentityTransformer
+from ballet.transformer import make_robust_transformer
 
 
-def get_target_encoder():
+def get_target_encoder(income_col='PINCP', income_threshold=50_000):
     """Get encoder for the prediction target
 
     Returns:
         transformer-like
     """
-    return IdentityTransformer()
+
+    # From Kaggle:
+    # > The prediction task is to determine whether a person makes over $50K a
+    # > year.
+    # Note: this is not adjusted for inflation since 1994
+    return make_robust_transformer([
+        lambda y: y[income_col],
+        lambda y: (y>income_threshold).astype(int),
+    ])
