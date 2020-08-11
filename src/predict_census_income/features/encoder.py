a87e81bb1cb98920d0296bc5afffe05f722b86f7
Micah Smith <micahs@mit.edu>
2020-08-11 17:06:46 -0400

    Adjust target for inflation

--- src/predict_census_income/features/encoder.py
+++ src/predict_census_income/features/encoder.py
@@ -2,7 +2,11 @@ from ballet.eng.misc import IdentityTransformer
 from ballet.transformer import make_robust_transformer
 
 
-def get_target_encoder(income_col='PINCP', income_threshold=50_000):
+# $50,000 in January 1994 adjusted via CPI-U to January 2018 dollars.
+INCOME_THRESHOLD = 84_770
+
+
+def get_target_encoder(income_col='PINCP', income_threshold=INCOME_THRESHOLD):
     """Get encoder for the prediction target
 
     Returns:
@@ -12,7 +16,7 @@ def get_target_encoder(income_col='PINCP', income_threshold=50_000):
     # From Kaggle:
     # > The prediction task is to determine whether a person makes over $50K a
     # > year.
-    # Note: this is not adjusted for inflation since 1994
+    # We adjust to 2018 dollars abovee.
     return make_robust_transformer([
         lambda y: y[income_col],
         lambda y: (y>income_threshold).astype(int),
