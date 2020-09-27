79cf60e8145d47bc1fda635f630fe2eab6246168
josepablocam <josepablocam@users.noreply.github.com>
2020-09-27 19:33:57 +0000

    Add new feature

---
+++ src/predict_census_income/features/contrib/user_josepablocam/feature_06da38d0_427a_44fc_98cf_8337e5790b9b.py
@@ -0,0 +1,7 @@
+from ballet import Feature
+
+input = ["OCCP"]  # TODO - str or list of str
+transformer = None  # TODO - function, transformer-like, or list thereof
+name = "Occupation"  # TODO - str
+description = "Occupation"  # TODO - str
+feature = Feature(input, transformer, name=name, description=description)
