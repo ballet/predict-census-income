8594b5c9006c775ecf246f802c8fd243d498ccb1
kelvin-lu <kelvin-lu@users.noreply.github.com>
2020-09-28 02:47:46 +0000

    Add new feature

---
+++ src/predict_census_income/features/contrib/user_kelvin_lu/feature_f47bd310_4092_4295_83f2_3f7849b1897c.py
@@ -0,0 +1,7 @@
+from ballet import Feature
+
+input = "FS"
+transformer = None
+name = "Food Stamp Recipient?"
+description = "Whether or not they recieve food stamps"
+feature = Feature(input, transformer, name=name, description=description)
