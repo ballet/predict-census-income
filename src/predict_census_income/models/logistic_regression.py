a5f888fabfd37431fae7e0238e0d31d62ea82501
Micah Smith <micahs@mit.edu>
2021-02-02 21:37:51 -0500

    Fix sample model

--- src/predict_census_income/models/logistic_regression.py
+++ src/predict_census_income/models/logistic_regression.py
@@ -1,9 +1,11 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 
 def create_logistic_regression_model(pipeline):
     return Pipeline([
         ('feature_engineering_pipeline', pipeline),
+        ('scaler', StandardScaler()),
         ('lr', LogisticRegression()),
     ])
